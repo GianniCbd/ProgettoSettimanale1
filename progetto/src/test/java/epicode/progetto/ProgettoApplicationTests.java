dfc41f986dbcf4affdc7632a4e4611cc32c95c0d
GianniCbd <giannicabiddu@gmail.com>
2024-02-09 16:54:21 +0100

    new upd LocationLibera

--- progetto/src/test/java/epicode/progetto/ProgettoApplicationTests.java
+++ progetto/src/test/java/epicode/progetto/ProgettoApplicationTests.java
@@ -1,13 +1,26 @@
 package epicode.progetto;
 
+import epicode.progetto.DAO.UtenteService;
+import epicode.progetto.entities.Utente;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProgettoApplicationTests {
 
+	@Autowired
+	private UtenteService utenteService;
+
 	@Test
-	void contextLoads() {
+	public void testSaveUtente() {
+		Utente utente = new Utente("username", "name", "email");
+		utenteService.saveUtente(utente);
+		Assertions.assertNotNull(utente.getId());
 	}
+
+
+
 
 }
